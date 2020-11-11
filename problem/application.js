077f4bbdef13f5f2b929afea7fa580631dbae532
Anderson Levasseur <anderson.levasseur@gmail.com>
2020-11-10 19:10:39 -0500

    Nov. 11

--- problem/application.js
+++ problem/application.js
@@ -1,4 +1,26 @@
 const Calculator = require("./calculator");
 
 let calc = new Calculator();
-// Fill in the operations described in the assignment below
+let hi 
+calc.getValue();
+calc.add(hi);
+calc.setCurrentValue(9);
+calc.getValue();
+console.log(' ');
+calc.add('Please dont work');
+calc.clearValue();
+calc.getValue();
+calc.add(9);
+calc.getRecentHistory();
+console.log('');
+
+calc.clearValue();
+calc.getRecentHistory();
+calc.toString();
+console.log('');
+calc.subtract(9);
+calc.multiplyBy(92);
+calc.divideBy(92);
+calc.getRecentHistory();
+calc.multiplyBy(12);
+calc.toString();
